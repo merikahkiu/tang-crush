fb4105fa46327a942544a06da2c048d205d56661
merikahkiu <86838103+merikahkiu@users.noreply.github.com>
2021-07-02 20:26:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'em bede à!',
+    introDesc: `chị êm mình chơi chung cũng gần 3 năm gòi  🥰 🥰  `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'nay chị cực khổ xách lon đi làm cái nì cho êm để hỏi cái nì 😙',
+    desc: 'sao êm cứ ngày càng thiểu năng z hã ',
+    btnYes: ' vẫn là tại êm pị thiểu năng <33',
+    btnNo: 'chắc tại chơi với chị đó :3',
+    question:'z chắc mình cũng sắp nghỉ chơi là dừa gồi em he <3',
+    btnReply: 'Gửi cho trị <3',
+    reply: 'hum đou tại trị đangiu qa nên mìn xẽ trơi quài :>>>>',
+    mess: 'Trị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'nghỉ trơi là m trết condimemay dới tao',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
